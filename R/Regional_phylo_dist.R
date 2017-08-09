03e3cd36340ba2bb8a565d06c470ee3eee72ce6a
levisc8 <levisc8@gmail.com>
2017-08-09 16:52:39 -0700

    Add regional phylogenetic dist function

---
+++ R/Regional_phylo_dist.R
@@ -0,0 +1,43 @@
+#' Make a species pool-level phylogenetic distance matrix
+#' 
+#' @description Create a phylogeny from a species pool and then calculate
+#' patristic distances between all species
+#' 
+#' @param spp.vector A character vector of species names. Species names
+#' must match those in phylogeny.
+#' @param phylo A larger phylogeny that contains all of the 
+#' species in the \code{spp.vector}. If this condition is not met,
+#' the function will try to merge in the missing species using
+#' \code{pez::congeneric.merge} while warning the user. 
+#' 
+#' @return A phylogenetic distance matrix in the form of a
+#' \code{data.frame}
+#' 
+#' @author Sam Levin
+#' 
+#' @importFrom magrittr %>%
+#' @importFrom ape cophenetic.phylo drop.tip
+#' @importFrom pez congeneric.merge
+#' @export
+
+make_regional_phylo_dist <- function(spp.vector, phylo){
+  spp.vector <- gsub("-","\\.",spp.vector)
+  
+  if(!all(spp.vector %in% phylo$tip.label)){
+    warning('Attempting merge missing species into tree.\n',
+            'Make sure to check output for accuracy.')
+    phylo <- pez::congeneric.merge(phylo, spp.vector)
+  }
+  
+  phylo <- ape::drop.tip(phylo, setdiff(phylo$tip.label, spp.vector))
+  bigDist <- ape::cophenetic.phylo(phylo) %>% data.frame()
+  
+  out <- bigDist[rownames(bigDist) %in% spp.vector, 
+                 names(bigDist) %in% spp.vector]
+  
+  if(dim(out)[1] != dim(out)[2]){
+    stop('resulting matrix is not square')
+  }
+  
+  return(out)
+}
